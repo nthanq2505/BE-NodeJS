cb6932000f1790e67b2d5a56b84e37221333c3d1
nthanq2505 <20127627@student.hcmus.edu.vn>
2024-07-31 21:08:54 +0700

    Create activities diagram for router and controller

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
 const { handlePing, handleHello } = require('./controller');
 
 function router(req, res) {
-    if (req.url === '/ping' && req.method === 'GET') {
+    if (req.method === 'GET' && req.url === '/ping' ) {
         handlePing(req, res);
     } else {
         handleHello(req, res);
